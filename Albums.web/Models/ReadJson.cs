afdb79146f3243949ac1d2a8017e021f7fc54643
Rafael <rcd99@zipmail.com.br>
2017-05-15 11:18:29 -0300

    Including missing file

---
+++ Albums.web/Models/ReadJson.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace Albums.web.Models
+{
+    public class ReadJson
+    {
+        public List<T> readUrl<T>(string url)
+        {
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            var serializer = new DataContractJsonSerializer(typeof(List<T>));
+
+            using (var ms = new MemoryStream(Encoding.Unicode.GetBytes(content)))
+            {
+                return (List<T>)serializer.ReadObject(ms);
+            }
+        }
+    }
+}
